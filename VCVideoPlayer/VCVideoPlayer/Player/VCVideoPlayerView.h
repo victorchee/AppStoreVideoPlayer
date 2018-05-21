d8af7f16e00d612d076739c4a900077bb44bc4e4
Victor Chee <victor.qhj@gmail.com>
2018-05-22 06:45:47 +0800

    Prepare for fullscreen.

--- VCVideoPlayer/VCVideoPlayer/Player/VCVideoPlayerView.h
+++ VCVideoPlayer/VCVideoPlayer/Player/VCVideoPlayerView.h
@@ -17,6 +17,7 @@ typedef NS_ENUM(NSUInteger, VCVideoPlayerFullscreenInterfaceOrientation) {
 
 @property (assign, nonatomic) VCVideoPlayerFullscreenInterfaceOrientation fullscreenInterfaceOrientation;
 @property (assign, readonly, nonatomic, getter=isFullscreen) BOOL fullscreen;
+@property (strong, nonatomic) UIView *fatherView;
 
 - (void)playVideoWithURL:(NSURL * _Nonnull)url controlView:(UIView  * _Nullable)controlView;
 
--- VCVideoPlayer/VCVideoPlayer/Player/VCVideoPlayerView.m
+++ VCVideoPlayer/VCVideoPlayer/Player/VCVideoPlayerView.m
@@ -10,7 +10,8 @@
 #import "VCVideoControlView.h"
 
 @interface VCVideoPlayerView() <VCVideoControlDelegate>
-
+@property (assign, readwrite, nonatomic, getter=isFullscreen) BOOL fullscreen;
+@property (strong, nonatomic) UIView *controlView;
 @end
 
 @implementation VCVideoPlayerView
@@ -27,15 +28,108 @@
     [controlView.leftAnchor constraintEqualToAnchor:self.leftAnchor].active = YES;
     [controlView.bottomAnchor constraintEqualToAnchor:self.bottomAnchor].active = YES;
     [controlView.rightAnchor constraintEqualToAnchor:self.rightAnchor].active = YES;
+    
+    self.controlView = controlView;
 }
 
 - (void)fullscreenActionAnimated:(BOOL)animated completion:(dispatch_block_t)completionHandler {
+    CGRect frameInWindow = [self convertRect:self.frame toView:nil];
+    [self removeFromSuperview];
+    [UIApplication.sharedApplication.keyWindow addSubview:self];
+    self.frame = frameInWindow;
+    self.controlView.alpha = 0;
     
+    if (animated) {
+        [UIView animateWithDuration:0.35 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+            [self exexuteFullscreen];
+        } completion:^(BOOL finished) {
+            if (completionHandler) {
+                completionHandler();
+            }
+            [UIView animateWithDuration:0.5 animations:^{
+                self.controlView.alpha = 1;
+            }];
+        }];
+    } else {
+        [self exexuteFullscreen];
+        if (completionHandler) {
+            completionHandler();
+        }
+        [UIView animateWithDuration:0.5 animations:^{
+            self.controlView.alpha = 1;
+        }];
+    }
+}
+
+- (void)exexuteFullscreen {
+    CGRect screenBounds = UIScreen.mainScreen.bounds;
+    CGPoint screenCenter = CGPointMake(CGRectGetMidX(screenBounds), CGRectGetMidY(screenBounds));
+    self.bounds = screenBounds;
+    self.center = screenCenter;
+    if (self.fullscreenInterfaceOrientation == VCVideoPlayerFullscreenInterfaceOrientationLandscape) {
+        self.transform = CGAffineTransformMakeRotation(M_PI_2);
+//        [UIApplication.sharedApplication setStatusBarOrientation:UIInterfaceOrientationLandscapeRight];
+    } else {
+//        [UIApplication.sharedApplication setStatusBarOrientation:UIInterfaceOrientationPortrait];
+    }
+}
+
+- (void)miniscreenActionAnimated:(BOOL)animated completion:(dispatch_block_t)completionHandler {
+    self.controlView.alpha = 0;
+    
+    if (animated) {
+        [UIView animateWithDuration:0.35 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+            [self exexuteMiniscreen];
+        } completion:^(BOOL finished) {
+            [self finishMiniscreen];
+            if (completionHandler) {
+                completionHandler();
+            }
+        }];
+    } else {
+        [self exexuteMiniscreen];
+        [self finishMiniscreen];
+        if (completionHandler) {
+            completionHandler();
+        }
+    }
+}
+
+- (void)exexuteMiniscreen {
+    CGRect screenBounds = UIScreen.mainScreen.bounds;
+    CGPoint screenCenter = CGPointMake(CGRectGetMidX(screenBounds), CGRectGetMidY(screenBounds));
+    self.bounds = screenBounds;
+    self.center = screenCenter;
+    if (self.fullscreenInterfaceOrientation == VCVideoPlayerFullscreenInterfaceOrientationLandscape) {
+        self.transform = CGAffineTransformMakeRotation(M_PI_2);
+        //        [UIApplication.sharedApplication setStatusBarOrientation:UIInterfaceOrientationLandscapeRight];
+    } else {
+        //        [UIApplication.sharedApplication setStatusBarOrientation:UIInterfaceOrientationPortrait];
+    }
+}
+
+- (void)finishMiniscreen {
+    [self removeFromSuperview];
+    [self.fatherView addSubview:self];
+    self.frame = self.fatherView.bounds;
+    [UIView animateWithDuration:0.5 animations:^{
+        self.controlView.alpha = 1;
+    }];
 }
 
 #pragma mark - VCVideoControlDelegate
 - (void)vcVideoControlView:(UIView *)controlView fullscreenAction:(UIButton *)sender {
-    [self fullscreenActionAnimated:YES completion:^{ }];
+    if (self.isFullscreen) {
+        // Miniscreen
+        [self miniscreenActionAnimated:YES completion:^{
+            self.fullscreen = NO;
+        }];
+    } else {
+        // Fullscreen
+        [self fullscreenActionAnimated:YES completion:^{
+            self.fullscreen = YES;
+        }];
+    }
 }
 
 @end
