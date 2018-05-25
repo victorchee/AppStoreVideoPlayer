cc101d6be47d96465c355f242080e810b5b6c825
Victor Chee <victor.qhj@gmail.com>
2018-05-25 10:31:07 +0800

    Add project.

---
+++ AppStoreVideoPlayer/AppStoreVideoPlayer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AppStoreVideoPlayer
+//
+//  Created by Migu on 2018/5/25.
+//  Copyright © 2018年 VIctorChee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- VCVideoPlayer/VCVideoPlayer/ViewController.m
+++ VCVideoPlayer/VCVideoPlayer/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 #import "VCVideoPlayerView.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    CGRect originalFrame;
+    CGRect frameInWindow;
+}
 @property (strong, nonatomic) IBOutlet UIView *contentView;
 @property (strong, nonatomic) VCVideoPlayerView *playerView;
 @end
@@ -19,16 +22,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.playerView = [[VCVideoPlayerView alloc] init];
-    self.playerView.backgroundColor = [UIColor purpleColor];
-    self.playerView.fatherView = self.contentView;
-    NSURL *url = [NSURL URLWithString:@"https://link"];
-    [self.playerView playVideoWithURL:url controlView:nil];
+//    self.playerView = [[VCVideoPlayerView alloc] init];
+//    self.playerView.backgroundColor = [UIColor purpleColor];
+//    self.playerView.fatherView = self.contentView;
+//    NSURL *url = [NSURL URLWithString:@"https://link"];
+//    [self.playerView playVideoWithURL:url controlView:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)fullscreenButtonTapped:(UIButton *)sender {
+    UIWindow *window = UIApplication.sharedApplication.keyWindow;
+    if (sender.isSelected) {
+        // Miniscreen
+    } else {
+        // Fullscreen
+        originalFrame = self.contentView.frame;
+        frameInWindow = [self.view convertRect:self.contentView.frame toView:nil];
+        self.contentView.frame = frameInWindow;
+        [window addSubview:self.contentView];
+        [UIView animateWithDuration:0.35 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+            self.contentView.transform = CGAffineTransformMakeRotation(M_PI_2);
+            self.contentView.bounds = CGRectMake(0, 0, CGRectGetHeight(self.contentView.superview.bounds), CGRectGetWidth(self.contentView.superview.bounds));
+            self.contentView.center = CGPointMake(CGRectGetMidX(self.contentView.superview.bounds), CGRectGetMidY(self.contentView.superview.bounds));
+        } completion:^(BOOL finished) {
+            [UIApplication.sharedApplication setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
+            self.contentView.frame = self.contentView.superview.bounds;
+            sender.selected = YES;
+        }];
+        //        [UIDevice.currentDevice setValue:@(UIInterfaceOrientationLandscapeRight) forKey:@"orientation"];
+        [UIApplication.sharedApplication setStatusBarOrientation:UIInterfaceOrientationLandscapeRight animated:YES];
+    }
 }
 
 /*
